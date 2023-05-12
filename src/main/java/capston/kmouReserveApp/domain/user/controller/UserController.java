bb5a0238d5253edaa2ccc3c9b84f6f0c1065d5ff
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-05-12 23:33:10 +0900

    UserController 생성

---
+++ src/main/java/capston/kmouReserveApp/domain/user/controller/UserController.java
@@ -0,0 +1,47 @@
+package capston.kmouReserveApp.domain.user.controller;
+
+import capston.kmouReserveApp.domain.user.dto.SignUpRequest;
+import capston.kmouReserveApp.domain.user.dto.SignUpResponse;
+import capston.kmouReserveApp.domain.user.dto.UserInfo;
+import capston.kmouReserveApp.domain.user.dto.UserRequest;
+import capston.kmouReserveApp.domain.user.service.LoginService;
+import capston.kmouReserveApp.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Log4j2
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    private final LoginService loginService;
+
+    // 회원가입
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(
+            @Valid @RequestBody SignUpRequest signUpRequest
+            ){
+        log.info("users signup, signupDto: {}",signUpRequest);
+        SignUpResponse signUpResponse = loginService.signUp(signUpRequest);
+
+        return new ResponseEntity<>(signUpResponse, HttpStatus.CREATED);
+    }
+
+    // 사용자 정보 조회
+    @GetMapping("")
+    public ResponseEntity<?> getUser(
+            @RequestBody UserRequest.UuidRequest uuidRequest
+            ){
+        String uuid = uuidRequest.getUuid();
+        UserInfo userInfo = userService.getUserByUuid(uuid);
+        log.info("userInfo: {}",userInfo);
+
+        return new ResponseEntity<>(userInfo,HttpStatus.OK);
+    }
+}
