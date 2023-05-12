15a23cdbc364787d6e237b1068414078189f3a86
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-05-12 20:27:11 +0900

    프로젝트 첫 세팅

---
+++ src/main/java/capston/kmouReserveApp/KmouReserveAppApplication.java
@@ -0,0 +1,13 @@
+package capston.kmouReserveApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmouReserveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmouReserveAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capston/kmouReserveApp/KmouReserveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package capston.kmouReserveApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmouReserveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
