b9934f0599f1ddad765d2d00f49b3589f7ba8122
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-05-12 21:59:59 +0900

    Room 엔티티 생성

--- src/main/java/capston/kmouReserveApp/domain/reservation/entity/Room.java
+++ src/main/java/capston/kmouReserveApp/domain/reservation/entity/Room.java
@@ -1,6 +1,39 @@
 package capston.kmouReserveApp.domain.reservation.entity;
 
 import capston.kmouReserveApp.domain.BaseTimeEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "room")
 public class Room extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Reservation> reservations = new ArrayList<>();
+
+    @Column(name = "max_num")
+    private int maxNum;
+
+    @Builder
+    public Room(Long id, String name, int maxNum) {
+        this.id = id;
+        this.name = name;
+        this.maxNum = maxNum;
+    }
 }
