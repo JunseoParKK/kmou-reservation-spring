16f6376d4790cd10fea25fc6cb45df26c0fbc421
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-05-12 23:00:21 +0900

    TimeParsingUtils 생성

---
+++ src/main/java/capston/kmouReserveApp/utils/TimeParsingUtils.java
@@ -0,0 +1,16 @@
+package capston.kmouReserveApp.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeParsingUtils {
+    public static LocalDateTime formatterLocalDateTime(String time){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return LocalDateTime.parse(time,formatter);
+    }
+
+    public static String formatterString(LocalDateTime time){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return time.format(formatter);
+    }
+}
