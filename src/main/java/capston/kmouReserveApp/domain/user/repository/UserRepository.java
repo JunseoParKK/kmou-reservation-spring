ca58eaf9f7416d2a65a92bef2fbece30b459e66f
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-05-12 21:39:28 +0900

    UserRepository findUserByIds 추가

--- src/main/java/capston/kmouReserveApp/domain/user/repository/UserRepository.java
+++ src/main/java/capston/kmouReserveApp/domain/user/repository/UserRepository.java
@@ -2,8 +2,16 @@ package capston.kmouReserveApp.domain.user.repository;
 
 import capston.kmouReserveApp.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    @Query("select u from User u where u.id =: userId")
+    List<UUID> findUserByIds(@Param("userId") UUID userId);
 }
