4574f5236615342e277d3c0802559ef1d905a8e4
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-05-12 20:45:13 +0900

    BaseTimeEntity 추가, @EnableJpaAuditing 추가

---
+++ src/main/java/capston/kmouReserveApp/BaseTimeEntity.java
@@ -0,0 +1,27 @@
+package capston.kmouReserveApp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity{
+
+    @CreatedDate
+    @JsonIgnore
+    @Column(name = "create_date")
+    private LocalDateTime createDate;
+
+    @CreatedDate
+    @JsonIgnore
+    @Column(name = "update_date")
+    private LocalDateTime modifiedDate;
+}
