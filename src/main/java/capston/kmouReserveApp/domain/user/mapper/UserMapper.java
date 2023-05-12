fffa78fbfc75efba0d81545f5a67b68334363cfc
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-05-12 22:40:03 +0900

    UserMapper SignUpResponse mapToDto 추가

--- src/main/java/capston/kmouReserveApp/domain/user/mapper/UserMapper.java
+++ src/main/java/capston/kmouReserveApp/domain/user/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package capston.kmouReserveApp.domain.user.mapper;
 
+import capston.kmouReserveApp.domain.user.dto.SignUpResponse;
 import capston.kmouReserveApp.domain.user.dto.UserInfo;
 import capston.kmouReserveApp.domain.user.entity.User;
 
@@ -12,6 +13,17 @@ public class UserMapper {
                 .password(savedUser.getPassword())
                 .phoneNum(savedUser.getPhoneNum())
                 .uuid(savedUser.getUuid())
+                .build();
+    }
+
+    public static SignUpResponse mapToDto(UserInfo userInfo){
+        return SignUpResponse.builder()
+                .id(userInfo.getId())
+                .name(userInfo.getName())
+                .email(userInfo.getEmail())
+                .password(userInfo.getPassword())
+                .phoneNum(userInfo.getPhoneNum())
+                .uuid(userInfo.getUuid())
                 .build();
     }
 }
