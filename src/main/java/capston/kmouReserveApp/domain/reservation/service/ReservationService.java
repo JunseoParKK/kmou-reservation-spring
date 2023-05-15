757959d7217c0fe70c7bcece0c3f0c2562c3e3f6
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-05-15 22:23:37 +0900

    ReservationController 예약 등록 추가

--- src/main/java/capston/kmouReserveApp/domain/reservation/service/ReservationService.java
+++ src/main/java/capston/kmouReserveApp/domain/reservation/service/ReservationService.java
@@ -1,4 +1,8 @@
 package capston.kmouReserveApp.domain.reservation.service;
 
+import capston.kmouReserveApp.domain.reservation.dto.ReservationDetails;
+import capston.kmouReserveApp.domain.reservation.dto.ReservationRequest;
+
 public interface ReservationService {
+    ReservationDetails register(String uuid, Long roomId, ReservationRequest.RegisterReservation registerReservation);
 }
