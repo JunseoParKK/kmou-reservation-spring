7381fde1ee9061f19bf1c85ba021bcbbe721c35a
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-05-15 22:16:28 +0900

    Reservation,Room Repository 생성

---
+++ src/main/java/capston/kmouReserveApp/domain/reservation/reservation/RoomRepository.java
@@ -0,0 +1,9 @@
+package capston.kmouReserveApp.domain.reservation.reservation;
+
+import capston.kmouReserveApp.domain.reservation.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room,Long> {
+}
