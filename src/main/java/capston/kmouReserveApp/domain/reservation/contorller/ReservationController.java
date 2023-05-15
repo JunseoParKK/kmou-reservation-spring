560e5dc037574d95f7cb44a609861f7c17be86c7
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-05-15 23:53:22 +0900

    ReservationController getReservation 추가

--- src/main/java/capston/kmouReserveApp/domain/reservation/contorller/ReservationController.java
+++ src/main/java/capston/kmouReserveApp/domain/reservation/contorller/ReservationController.java
@@ -1,5 +1,6 @@
 package capston.kmouReserveApp.domain.reservation.contorller;
 
+import capston.kmouReserveApp.auth.AuthUser;
 import capston.kmouReserveApp.domain.reservation.dto.ReservationDetails;
 import capston.kmouReserveApp.domain.reservation.dto.ReservationRequest;
 import capston.kmouReserveApp.domain.reservation.service.ReservationService;
@@ -8,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Log4j2
 @RestController
@@ -38,5 +36,17 @@ public class ReservationController {
         return new ResponseEntity<>(reservationService.register(
                 roomId,registerReservation), HttpStatus.CREATED
         );
+    }
+
+    /*
+     * 등록한 예약 상세 보기
+     */
+    @GetMapping("/room/{roomId}/reservation/{reservationToken}")
+    public ResponseEntity<ReservationDetails>getReservation(
+            @PathVariable("roomId") Long roomId,
+            @PathVariable("reservationToken") String reservationToken
+    ){
+        log.info("reservation getById, roomId: {}, reservationToken: {}",roomId,reservationToken);
+        return ResponseEntity.ok(reservationService.getByRoomAndReservationToken(roomId,reservationToken));
     }
 }
