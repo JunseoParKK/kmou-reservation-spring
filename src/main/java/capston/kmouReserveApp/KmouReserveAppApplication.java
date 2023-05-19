662b491b0f4e081d40ea9faff5436f12ef31ca95
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-05-19 14:53:00 +0900

    build.gradle 설정 추가

--- src/main/java/capston/kmouReserveApp/KmouReserveAppApplication.java
+++ src/main/java/capston/kmouReserveApp/KmouReserveAppApplication.java
@@ -2,14 +2,13 @@ package capston.kmouReserveApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class KmouReserveAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KmouReserveAppApplication.class, args);
 	}
-
 }
