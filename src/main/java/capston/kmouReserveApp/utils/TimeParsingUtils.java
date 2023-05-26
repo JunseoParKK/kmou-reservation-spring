ef9e1b2e71eeaf05f524bf0296416b25562db40c
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-05-26 22:09:19 +0900

    TimeParsingUtils formatterDate 추가

--- src/main/java/capston/kmouReserveApp/utils/TimeParsingUtils.java
+++ src/main/java/capston/kmouReserveApp/utils/TimeParsingUtils.java
@@ -1,7 +1,10 @@
 package capston.kmouReserveApp.utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class TimeParsingUtils {
     public static LocalDateTime formatterLocalDateTime(String time){
@@ -12,5 +15,19 @@ public class TimeParsingUtils {
     public static String formatterString(LocalDateTime time){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return time.format(formatter);
+    }
+
+    public static Date formatterDate(String dateFormat) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = simpleDateFormat.parse(dateFormat);
+        return date;
+    }
+
+    public static Date LocalToDate(LocalDateTime dateTime) throws ParseException{
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String dateFormat = formatterString(dateTime);
+        dateFormat = dateFormat.substring(0,10);
+        Date date = simpleDateFormat.parse(dateFormat);
+        return date;
     }
 }
